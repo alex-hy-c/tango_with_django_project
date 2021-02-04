1f8af8bd797bd5338f6d4c1c8c7ff47007a04b13
Alex Chak <2558782c@student.gla.ac.uk>
2021-02-04 12:20:33 +0000

    5 w exercise

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
 	#Construct dictionary
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-	#return rendered
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {}
+	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+	context_dict['categories'] = category_list
+	# Render the response and send it back!
 	return render(request, 'rango/index.html', context=context_dict)
 
