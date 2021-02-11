d02e6983e9082011522af492a0787c241094f940
Alex Chak <2558782c@student.gla.ac.uk>
2021-02-11 22:42:50 +0000

    6

--- rango/urls.py
+++ rango/urls.py
@@ -6,6 +6,10 @@ from django.conf.urls.static import static
 app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+path('about/', views.about, name='about'),
+path('category/<slug:category_name_slug>/',
+	views.show_category, name='show_category'),
+]
+
 
 
